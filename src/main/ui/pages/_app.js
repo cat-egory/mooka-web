e53234f8f83cd9c9796c41c7854d2b58a6aa92d5
cat-egory <xtheblackeyes@gmail.com>
2022-05-09 23:00:29 +0900

    feat : 메인화면 문구와 이미지 셋팅

--- src/main/ui/pages/_app.js
+++ src/main/ui/pages/_app.js
@@ -9,12 +9,12 @@ import PageChange from 'components/PageChange/PageChange.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'styles/tailwind.css';
 
-Router.events.on('routeChangeStart', (url) => {
+Router.events.on('routeChangeStart', url => {
   console.log(`Loading: ${url}`);
   document.body.classList.add('body-page-transition');
   ReactDOM.render(
     <PageChange path={url} />,
-    document.getElementById('page-transition')
+    document.getElementById('page-transition'),
   );
 });
 Router.events.on('routeChangeComplete', () => {
@@ -72,8 +72,7 @@ export default class MyApp extends App {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title>Notus NextJS by Creative Tim</title>
-          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+          <title>MooKA - Movie, BooK, DramA</title>
         </Head>
         <Layout>
           <Component {...pageProps} />
--- src/main/ui/pages/index.js
+++ src/main/ui/pages/index.js
@@ -11,48 +11,38 @@ export default function Index() {
       <IndexNavbar fixed />
       <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
         <div className="container mx-auto items-center flex flex-wrap">
-          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4 z-10">
             <div className="pt-32 sm:pt-0">
               <h2 className="font-semibold text-4xl text-blueGray-600">
-                Notus NextJS - my test
+                감상한 영화, 책, 드라마를 기록하는 공간
               </h2>
-              <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                Notus NextJS is Free and Open Source. It does not change any of
-                the CSS from{' '}
-                <a
-                  href="https://tailwindcss.com/?ref=creativetim"
-                  className="text-blueGray-600"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Tailwind CSS
-                </a>
-                . It features multiple HTML elements and it comes with dynamic
-                components for ReactJS, Vue and Angular.
+              <p className="mt-4 text-lg leading-relaxed text-gray-500">
+                평소 본 영화, 책, 드라마들을 기록하고 연말에 회고할 때 편한
+                서비스가 없을까 해서 만든 사이트
               </p>
               <div className="mt-12">
                 <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/overview/notus?ref=nnjs-index"
+                  href="/login"
                   target="_blank"
                   className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-400 active:bg-blueGray-500 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
                 >
-                  Get started
+                  로그인
                 </a>
                 <a
-                  href="https://github.com/creativetimofficial/notus-nextjs?ref=nnjs-index"
+                  href="/home"
                   className="github-star ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg"
                   target="_blank"
                 >
-                  Github Star
+                  둘러보기
                 </a>
               </div>
             </div>
           </div>
         </div>
         <img
-          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
-          src="/img/pattern_nextjs.png"
-          alt="..."
+          className="absolute top-95-px b-auto right-0 pt-16 sm:w-6/12 w-10/12 -mt-48 sm:mt-0 max-h-860-px z-2 opacity-80"
+          src="/img/chris-bair-S8FOtAmjJYY-unsplash.jpg"
+          alt="mooka front image"
         />
       </section>
 
