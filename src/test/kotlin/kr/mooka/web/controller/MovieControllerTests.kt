36f1c2552be863d4174475e32ff4a3dd0144d872
cat-egory <xtheblackeyes@gmail.com>
2022-05-23 23:00:13 +0900

    feat : 무비 controller 테스트 고도화

--- src/test/kotlin/kr/mooka/web/controller/MovieControllerTests.kt
+++ src/test/kotlin/kr/mooka/web/controller/MovieControllerTests.kt
@@ -1,21 +1,26 @@
 package kr.mooka.web.controller
 
+import kr.mooka.web.dto.MovieInfo
 import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MovieControllerTests(@Autowired val restTemplate: TestRestTemplate) {
 
     @Test
     fun `Get Random Movie List`() {
-        val entity = restTemplate.getForEntity<String>("/movie/random")
-        println("entity:$entity")
-        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-//        assertThat(entity.body).contains("<h1>Blog</h1>")
+        val response: ResponseEntity<Array<MovieInfo>> = restTemplate.getForEntity(
+            "/movie/random?limit=8",
+            Array<MovieInfo>::class.java
+        )
+        val movieList: Array<MovieInfo> = response.body!!
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(movieList.size).isEqualTo(3)
     }
 }
